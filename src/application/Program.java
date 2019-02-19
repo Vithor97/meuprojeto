855f549ad9234422b77180fe80ce57adf37c94da
vithor97 <vitorsantos27@hotmail.com>
2019-02-18 21:26:01 -0300

    Primeiro projeto

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+	
+	private static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world");		
+
+	}
+
+}
